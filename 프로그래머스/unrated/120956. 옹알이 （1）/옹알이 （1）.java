c9996ec0abf0ce2d3cd455bd15382d0d85255ada
taewoo Lim <105422037+Tesssssssssy@users.noreply.github.com>
2023-11-22 18:51:11 +0900

    [level unrated] Title: 옹알이 (1), Time: 1.56 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,16 +1,17 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-
+        
         for(int i = 0; i < babbling.length; i++) {
-            // replaceFirst는 자신이 바꾸고 싶은 문자열이 처음이라면 그 문자만 치환하고 끝난다.
-            babbling[i] = babbling[i].replaceFirst("aya", "0");
-            babbling[i] = babbling[i].replaceFirst("woo", "0");
-            babbling[i] = babbling[i].replaceFirst("ye", "0");
-            babbling[i] = babbling[i].replaceFirst("ma", "0");
-            babbling[i] = babbling[i].replace("0", "");
+            babbling[i] = babbling[i].replaceFirst("aya", "2");
+            babbling[i] = babbling[i].replaceFirst("ye", "2");
+            babbling[i] = babbling[i].replaceFirst("woo", "2");
+            babbling[i] = babbling[i].replaceFirst("ma", "2");
+            babbling[i] = babbling[i].replace("2", "");
             
-            if(babbling[i].equals("")) answer++;
+            if (babbling[i].equals("")) {
+                answer++;
+            }
         }
         
         return answer;
