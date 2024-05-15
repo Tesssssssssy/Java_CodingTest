6b528602a8e72cb1993319b64512ccf693c0135d
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-05-16 05:23:08 +0900

    [level 2] Title: 타겟 넘버, Time: 0.43 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,27 +2,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
+    private static int answer = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            int size = queue.size();
-            for (int j = 0; j < size; j++) {
-                Integer sum = queue.poll();
-                queue.add(sum + numbers[i]);
-                queue.add(sum - numbers[i]);
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            Integer sum = queue.poll();
-
-            if (sum == target) {
-                answer += 1;
-            }
-        }
+    public static int solution(int[] numbers, int target) {
+        answer = 0;
+        dfs(numbers, target, 0, 0);
         return answer;
+    }
+
+    private static void dfs(int[] numbers, int target, int index, int currentSum) {
+        if (index == numbers.length) {
+            if (currentSum == target) {
+                answer++;
+            }
+            return;
+        }
+
+        // 현재 숫자를 더하는 경우
+        dfs(numbers, target, index + 1, currentSum + numbers[index]);
+        // 현재 숫자를 빼는 경우
+        dfs(numbers, target, index + 1, currentSum - numbers[index]);
     }
 }
