4645d1814a66f0728749c34503350f3cab692f15
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-05-31 01:27:28 +0900

    [level 2] Title: 전화번호 목록, Time: 278.72 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
         Arrays.sort(phone_book);
 
-        // 정렬된 목록에서 인접한 요소끼리 접두어 관계인지 검사
+        // 인접한 요소끼리 접두어 관계인지 확인
         for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
             }
         }
 
-        // 접두어 관계가 없으면 true 반환
-        return true;
+        return answer;
     }
 }
