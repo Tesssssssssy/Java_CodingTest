bd676c5242ae09a6144e9fa908c555c2006f42f5
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-05-31 03:13:19 +0900

    [level 2] Title: 올바른 괄호, Time: 19.50 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 class Solution {
-    boolean solution(String s) {
+    public static boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
