49a417c945b9aad7217356079f3b6a9ce431dcaf
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-05-31 15:44:45 +0900

    [level 2] Title: 전화번호 목록, Time: 239.34 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public static boolean solution(String[] phone_book) {
         // 해시맵을 사용하여 전화번호 목록을 저장.
-        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> map = new HashSet<>();
 
         // 각 전화번호를 해시맵에 추가.
         // 각 번호를 키로 사용하고, 값으로 1을 설정.
         for (String number : phone_book) {
-            map.put(number, 1);
+            map.add(number);
         }
 
         // 각 전화번호에 대해 접두사가 다른 전화번호의 접두사로 존재하는지 확인.
@@ -16,9 +16,9 @@ class Solution {
             // 전화번호의 각 접두사를 확인. (1자리부터 n-1자리까지)
             for (int i = 1; i < number.length(); i++) {
                 // 현재 접두사가 해시맵에 키로 존재하면, 다른 전화번호의 접두사.
-                if (map.containsKey(number.substring(0, i))) {
+                if (map.contains(number.substring(0, i))) {
                     // 접두사가 존재한다는 것 출력. (디버깅용)
-                    System.out.println(map.containsKey(number.substring(0, i)) + ", " + i);
+                    System.out.println(map.contains(number.substring(0, i)) + ", " + i);
                     // 접두사가 존재하면 false 반환.
                     return false;
                 }
