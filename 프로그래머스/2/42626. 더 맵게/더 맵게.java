13d214bee67dd3d60a4790c19b0231a023462cbf
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-06-03 22:40:09 +0900

    [level 2] Title: 더 맵게, Time: 1666.02 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,30 +1,32 @@
 import java.util.PriorityQueue;
 
 class Solution {
+    /**
+     *  1. 입력받은 스코빌 배열 PriorityQueue에 넣어서 자동 정렬
+     *  2. 가장 작은 값 2개 선택 후
+     *     새로운 값 = 가장 작은 값 + ( 두 번째로 작은 값 * 2 )
+     *  3. 이 때 반환할 answer++
+     *  4. 새로운 값을 다시 PriorityQueue에 넣는다.
+     *  5. 모든 값이 K 이상이면 answer return
+     */
     public static int solution(int[] scoville, int K) {
         int answer = 0;
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
         for (int scov : scoville) {
-            minHeap.offer(scov);
+            priorityQueue.offer(scov);
         }
-        // 입력받은 스코빌 정수 배열을 PriorityQueue에 저장
 
-        while (minHeap.size() > 1 && minHeap.peek() < K) {
-            int firstMin = minHeap.poll();
-            int secondMin = minHeap.poll();
-
-            int mixedScoville = firstMin + (secondMin * 2);
-            minHeap.offer(mixedScoville);
+        //while (priorityQueue.peek() < K) { // test 1, 3, 8, 14 - 실패(런타임 에러)
+        while (priorityQueue.size() > 1 && priorityQueue.peek() < K) {
+            int min1 = priorityQueue.poll();
+            int min2 = priorityQueue.poll();
+            int newValue = min1 + (min2 * 2);
             answer++;
+            priorityQueue.offer(newValue);
         }
 
-        /*
-            이 떄, 만약 최소 값이 K보다 넘지 않으면
-            모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우이므로 -1 반환
-        */
-        if (minHeap.peek() < K) {
+        if (priorityQueue.peek() < K) {
             return -1;
         }
 
