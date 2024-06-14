17130e512995d0df23e6f5a7dc53aecc0b759609
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-06-14 14:32:11 +0900

    [level 3] Title: 등굣길, Time: 0.60 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,32 +1,30 @@
 class Solution {
-    static int MOD = 1_000_000_007;
-
     public static int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n+1][m+1];
+        int[][] street = new int[n][m];
 
-        // 물에 잠긴 지역을 -1로 설정
-        for (int[] puddle : puddles) {
-            dp[puddle[1]][puddle[0]] = -1;
-        }
+        // 웅덩이는 -1
+        for (int[] puddle : puddles)
+            street[puddle[1] - 1][puddle[0] - 1] = -1;
 
-        // 시작 위치 초기화
-        dp[1][1] = 1;
+        street[0][0] = 1;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (dp[i][j] == -1) {
-                    dp[i][j] = 0; // 물에 잠긴 지역은 경로 개수를 0으로 설정
+        for (int i = 0; i < n; i++) { // 시작점은 1로 저장
+            for (int j = 0; j < m; j++) {
+
+                if (street[i][j] == -1) { // 웅덩이면 0으로 바꾸고 continue
+                    street[i][j] = 0;
                     continue;
                 }
-                if (i > 1) {
-                    dp[i][j] = (dp[i][j] + dp[i-1][j]) % MOD;
-                }
-                if (j > 1) {
-                    dp[i][j] = (dp[i][j] + dp[i][j-1]) % MOD;
-                }
+
+                if (i != 0)
+                    street[i][j] += street[i - 1][j] % 1000000007;
+                // 숫자가 이 값을 초과할 수 있기 때문에 계산 과정에서 나머지 구하기
+
+                if (j != 0)
+                    street[i][j] += street[i][j - 1] % 1000000007; // 왼쪽
             }
         }
 
-        return dp[n][m];
+        return street[n - 1][m - 1] % 1000000007;
     }
 }
