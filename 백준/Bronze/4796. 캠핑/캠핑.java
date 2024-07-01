7ec1dc606a6f0d34bf72eee61adb08dc9d673303
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-07-01 09:33:22 +0900

    [Bronze I] Title: 캠핑, Time: 156 ms, Memory: 16448 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNumber = 1;
+
+        while (true) {
+            String[] input = br.readLine().split(" ");
+            int L = Integer.parseInt(input[0]);
+            int P = Integer.parseInt(input[1]);
+            int V = Integer.parseInt(input[2]);
+
+            if (L == 0 && P == 0 && V == 0)
+                break;
+
+            // 캠핑 가능 일수 계산
+            int fullCycles = V / P;
+            int remainingDays = V % P;
+            int maxUsableDays = fullCycles * L + Math.min(remainingDays, L);
+
+            System.out.println("Case " + caseNumber + ": " + maxUsableDays);
+            caseNumber++;
+        }
+    }
+}
