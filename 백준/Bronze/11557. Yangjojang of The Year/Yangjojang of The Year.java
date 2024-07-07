d38d4df4dada135a066e84f7852879e58e406d50
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-07-07 21:40:11 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 1. 가장 술 소비가 많은 학교 이름 출력
+     * 2. 1) 입력 첫 줄: 테스트 케이스의 숫자 T
+     * 2) 매 입력의 첫 줄에는 학교의 숫자 정수 N
+     * 3) N줄에 걸쳐 학교 이름, 한 해 소비한 술의 양
+     * 3. 소비한 술의 양이 같은 학교는 없다.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());  // 각 테스트 케이스에서의 학교 수
+
+            Map<String, Integer> schoolAlcohol = new TreeMap<>();
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String schoolName = st.nextToken();
+                int alcoholAmount = Integer.parseInt(st.nextToken());
+                schoolAlcohol.put(schoolName, alcoholAmount);
+            }
+
+            // TreeMap에서 value로 최대값 찾고
+            int maxAlcoholAmount = Collections.max(schoolAlcohol.values());
+
+            // 찾은 maxDrunkenSchool 값을 갖는 키를 찾음
+            for (Map.Entry<String, Integer> entry : schoolAlcohol.entrySet()) {
+                if (entry.getValue() == maxAlcoholAmount) {
+                    System.out.println(entry.getKey());
+                    break;
+                }
+            }
+        }
+    }
+}
