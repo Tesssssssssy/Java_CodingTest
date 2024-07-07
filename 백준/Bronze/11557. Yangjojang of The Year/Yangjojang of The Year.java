0341859d6d36576d85fd306a76ed5928bef1c29c
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-07-07 21:56:27 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.java
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.java
@@ -17,7 +17,7 @@ public class Main {
             int N = Integer.parseInt(br.readLine());  // 각 테스트 케이스에서의 학교 수
 
             /*
-                문제에서 소비한 술의 양이 같은 학교가 없다고 했으므로 술의 양을 key로 한다.
+                문제에서 소비한 술의 양이 같은 학교가 없다고 했으므로 술의 양을 key 로 한다.
                 TreeMap 자료구조를 사용할 것이고 자동으로 키 값을 기준으로 정렬이 되기 때문에
                 해당하는 키 값으로 학교 이름(value)을 찾는다.
             */
@@ -30,7 +30,31 @@ public class Main {
                 schoolAlcohol.put(alcoholAmount, schoolName);
             }
 
+            /*
+                술의 양과 학교이름을 TreeMap 에 넣고
+                TreeMap 의 lastKey 로 최대값을 갖는 키를 정렬 없이 찾고
+                그리고 최종적으로 그 키 값으로 TreeMap 에서 해당하는 value 를 찾아서 출력한다.
+            */
             System.out.println(schoolAlcohol.get(schoolAlcohol.lastKey()));
+            
+            /*
+                [입력]
+                Yonsei 10
+                Korea 10000000
+                Ewha 20
+                
+                [저장]
+                <10, Yonsei>
+                <10000000. Korea>
+                <20, Ewha>
+
+                [출력]
+                schoolAlcohol.firstEntry() - 출력: 10=Yonsei
+                schoolAlcohol.firstKey()   - 출력: 10
+
+                schoolAlcohol.lastEntry()  - 출력: 10000000=Korea
+                schoolAlcohol.lastKey()    - 출력: 10000000
+            */
         }
     }
 }
