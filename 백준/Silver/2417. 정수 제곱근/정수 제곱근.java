dfed49e7dfb4f850d991fd7797b2f04956643b57
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-07-23 15:28:41 +0900

    [Silver IV] Title: 정수 제곱근, Time: 112 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -1,15 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main{
+public class Main {
+    /**
+     * 1. 정수가 주어지면, 정수 제곱근을 구하라.
+     *
+     * [입력]
+     * 정수 n
+     *
+     * [출력]
+     * q^2 >= n인 가장 작은 음이 아닌 정수 q를 출력
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         long n = Long.parseLong(br.readLine());
-        long q = (long)Math.sqrt(n);
+        long q = (long) Math.sqrt(n);
 
-        if((q*q) < n) q++;
+        if ((q * q) < n)
+            q++;
 
         System.out.println(q);
     }
