3e8574ea73e0deff25e486bd5749bea6ac97408c
Tesssssssssy <105422037+Tesssssssssy@users.noreply.github.com>
2024-08-13 01:52:13 +0700

    [Silver III] Title: 두 수의 합, Time: 344 ms, Memory: 25564 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -14,14 +14,14 @@ public class Main {
      *
      *  [출력]
      *  문제의 조건을 만족하는 쌍의 개수를 출력한다.
-     *  
+     *
      *  [풀이]
-     *  HashMap
+     *  투 포인터
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());  // 수열의 크기
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -31,21 +31,26 @@ public class Main {
 
         int x = Integer.parseInt(br.readLine());  // 주어진 합 x
 
-        // 수의 빈도를 저장할 HashMap
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;  // 조건을 만족하는 쌍의 개수
+        // 배열 오름차순 정렬
+        Arrays.sort(arr);
 
-        for (int i = 0; i < n; i++) {
-            int current = arr[i];
-            int target = x - current;
+        int left = 0;
+        int right = n - 1;
+        int count = 0;
 
-            // 현재 수와 합쳐서 x가 되는 수가 이미 map에 있다면, 해당 개수만큼 쌍이 존재
-            if (map.containsKey(target)) {
-                count += map.get(target);
+        // 투 포인터로 두 수의 합이 x가 되는 쌍의 개수를 찾음
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == x) {
+                count++;
+                left++;
+                right--;
+            } else if (sum < x) {
+                left++;
+            } else {
+                right--;
             }
-
-            // 현재 수를 map에 추가 (현재 수의 등장 횟수 증가)
-            map.put(current, map.getOrDefault(current, 0) + 1);
         }
 
         System.out.println(count);
